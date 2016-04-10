4bb1428b4e184c1878aeb42f6639994794409c7a
pythonology <pythonology@gmail.com>
2016-04-10 10:17:52 -0400

    disco: New logging format, and support for log-level config variable

--- disco/__init__.py
+++ disco/__init__.py
@@ -1,9 +1,15 @@
 import json
+import logging
 
 from disco import discobot
 
 
 with open('config.json', 'r') as f:
     config = json.load(f)
+
+format = '%(asctime)s ::%(levelname)s:: %(message)s'
+datefmt = '%Y-%m-%d %H:%M:%S'
+level = getattr(logging, config.get('log-level', 'info').upper())
+logging.basicConfig(format=format, datefmt=datefmt, level=level)
 
 bot = discobot.DiscoBot()
