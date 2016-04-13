898eb2f2d2409257798fc3caadb3de1c98e3169a
pythonology <pythonology@gmail.com>
2016-04-13 11:40:51 -0400

    disco: Refactor play() to make things a bit easier for later

--- disco/commands.py
+++ disco/commands.py
@@ -9,18 +9,18 @@ async def play(uri: str):
     # TODO: Use regular expressions when validating each URL.
     if 'spotify' in uri:
         bot.service = constants.SPOTIFY_SERVICE
-    else:
-        match = re.match(constants.RE_ATTACHMENT_URI, uri)
-        if match is None:
-            await bot.say('Invalid URI.')
-            return
+        return
 
+    match = re.match(constants.RE_ATTACHMENT_URI, uri)
+    if match is not None:
         author_name = match.group(1)
         filename = match.group(2)
 
         path = os.path.join('attachments', author_name, filename)
         if not os.path.exists(path):
-            await bot.say('This attachment does not exist!')
+            await bot.say('That attachment does not exist!')
             return
 
         await bot.say("I can't do that yet.")
+
+    await bot.say('Invalid URI.')
