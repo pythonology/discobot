ca3c4cd411b92b97a9648044ddccc3cf84a64365
pythonology <pythonology@gmail.com>
2016-04-19 15:15:11 -0400

    __main__: Allow users to specify the path to their config file if needed

--- disco/__main__.py
+++ disco/__main__.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Disco: a fully-featured music bot for Discord"""
+
+import argparse
 import yaml
 import logging
 
@@ -7,8 +10,16 @@ from discord import opus
 from disco import bot
 
 if __name__ == '__main__':
-    with open('config.yaml') as f:
-        config = yaml.load(f)
+    parser = argparse.ArgumentParser(
+        prog='disco', description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        'config', nargs='?', type=argparse.FileType('r'),
+        default='config.yaml', help='configuration file to use')
+    args = parser.parse_args()
+
+    config = yaml.load(args.config.read())
+    args.config.close()
 
     kwargs = config.get('log', {}).copy()
     level = getattr(logging, kwargs.pop('level', 'info').upper(), logging.INFO)
